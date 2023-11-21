811654832365cd3834d0dad29619de7dd2654a6f
AdrianAmaral20 <adri13amaral@gmail.com>
2023-11-21 15:50:38 -0300

    adicionado evento de click em todos os elementos do teclado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ let descricao = document.querySelector('.d-1-4');
 let aviso = document.querySelector('.d-2');
 let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
+
+function clicou(n) {
+  alert(`Clicou em ${n}`)
+}
+
+function branco() {
+  alert(`Clicou em BRANCO`)
+}
+
+function corrige(n) {
+  alert(`Clicou em CORRIGE`)
+}
+
+function confirma(n) {
+  alert(`Clicou em CONFIRMA`)
+}
