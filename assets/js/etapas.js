5d80d4988c69c1e76a9414dcdc29d1e9102caa97
AdrianAmaral20 <adri13amaral@gmail.com>
2023-11-21 12:12:49 -0300

    json dos personagens criados

---
+++ assets/js/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: 'Jairzinho do gás',
+        partido: 'ABC',
+        fotos: [
+          {url:'38111.jpg', legenda: 'Vereador', small: false}
+        ]
+      },
+      {
+        numero: 77222,
+        nome: 'Zé da Padaria',
+        partido: 'CBA',
+        fotos: [
+          {url:'77222.jpg', legenda: 'Vereador'}
+        ]
+      },
+    ]
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: 'Sebastião',
+        partido: 'ABC',
+        vice: 'Jair',
+        fotos: [
+          {url:'99.jpg', legenda: 'Prefeito'},
+          {url:'99_2.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+      {
+        numero: 84,
+        nome: 'Antônio',
+        partido: 'PINGA',
+        vice: 'Ze',
+        fotos: [
+          {url:'84.jpg', legenda: 'Prefeito'},
+          {url:'84_2.jpg', legenda: 'Vice-Prefeito', small: true}
+        ]
+      },
+    ]
+  }
+]
