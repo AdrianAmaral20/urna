22e159c7312266fd3ea13b848d8b982d974866c0
AdrianAmaral20 <adri13amaral@gmail.com>
2023-11-21 16:16:59 -0300

    finalizada a função clicou, adicionando numeros na tela e em variavel

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,20 @@ let lateral = document.querySelector('.d-1-right');
 let numeros = document.querySelector('.d-1-3');
 
 let etapaAtual = 0;
+let numero = '';
 
 function comecarEtapa() {
   let etapa = etapas[etapaAtual];
 
   let numeroHtml = '';
+  
+  for(let i=0;i<etapa.numeros;i++) {
+    if(i === 0) {
+      numeroHtml += '<div class="numero pisca"></div>';
+    } else {
+      numeroHtml += '<div class="numero"></div>';
+    }
+  }
 
   seuVotoPara.style.display = 'none';
   cargo.innerHTML = etapa.titulo;
@@ -20,8 +29,23 @@ function comecarEtapa() {
   numeros.innerHTML = numeroHtml;
 }
 
+function atualizaInterface() {
+
+}
+
 function clicou(n) {
-  alert(`Clicou em ${n}`)
+  let elNumero = document.querySelector('.numero.pisca');
+  if(elNumero !== null) {
+    elNumero.innerHTML = n;
+    numero = `${numero}${n}`;
+
+    elNumero.classList.remove('pisca');
+    if(elNumero.nextElementSibling !== null) {
+      elNumero.nextElementSibling.classList.add('pisca')
+    } else {
+      atualizaInterface();
+    }
+  }
 }
 
 function branco() {
@@ -35,3 +59,5 @@ function corrige(n) {
 function confirma(n) {
   alert(`Clicou em CONFIRMA`)
 }
+
+comecarEtapa();
